72126dc5a1bd1bda9f9187c9ed9bec91813998e9
viniciszs <114860862+viniciszs@users.noreply.github.com>
2024-02-10 14:34:33 -0300

    create endpoit user

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import express from 'express'
-
+import { routerUser } from './router/user'
 const port = 8182
 
 const server = express()
 
 server.use(express.json())
+server.use('/user', routerUser)
 
 
 server.listen(port, () => { 
-  console.log(`API running port: ${port}`)
+  console.log(`API running url http://localhost:${port}`)
 })
